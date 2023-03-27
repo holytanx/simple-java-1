e7454bb3efdb96215982c4e13293f43e66c74f94
yong <tanyarat.tcm@gmail.com>
2023-03-27 11:15:04 +0700

    Test numeric cast case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String name = "John Doe";
 
         // BAD: Unnecessary call to 'toString' on 'name'
         System.out.println("Hi, my name is " + name.toString());
 
+        long data;
+
+        BufferedReader readerBuffered = new BufferedReader(
+                new InputStreamReader(System.in, "UTF-8"));
+        String stringNumber = readerBuffered.readLine();
+        if (stringNumber != null) {
+            data = Long.parseLong(stringNumber.trim());
+        } else {
+            data = 0;
+        }
+
+        // AVOID: potential truncation if input data is very large,
+        // for example 'Long.MAX_VALUE'
+        int scaled = (int)data;
     }
 }
